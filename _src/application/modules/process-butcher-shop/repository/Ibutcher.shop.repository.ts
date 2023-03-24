40d03caf3ca3810645d7f02b5906ee01119b045d
Carlos Magnani <unicornio26662@gmail.com>
2023-03-23 22:42:53 -0300

    deleted files

--- _src/application/modules/process-butcher-shop/repository/Ibutcher.shop.repository.ts
+++
@@ -1,6 +0,0 @@
-import { HttpResponse } from "../../../../core/types/Http.response";
-import { ButcherShop } from "../entity/butcher.shop.entity";
-
-export interface IbutcherShopRepository {
-    save(process: ButcherShop): Promise<HttpResponse | void>
-}
