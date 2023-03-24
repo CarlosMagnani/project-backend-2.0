5b8423a60893046e32c68941127be7d01c5d9198
Carlos Magnani <unicornio26662@gmail.com>
2023-03-23 22:42:29 -0300

    fix(test): fix the test files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     //To use environment variables in test files
-    setupFiles: './src/test/environment-variables-config.ts',
+    setupFiles: './_src/test/environment-variables-config.ts',
   },
 });
